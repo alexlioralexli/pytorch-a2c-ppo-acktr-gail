fc7866fae1727ae5c75acb0e9df7efa379265430
Alex Li <alexlioralexli@gmail.com>
2021-02-02 20:29:35 -0800

    moving exps to other machines

--- experiment_scripts/2-2-mujoco/2-2.py
+++ experiment_scripts/2-2-mujoco/2-2.py
@@ -1,5 +1,5 @@
 total = 0
-envs = ['Ant-v2', 'Humanoid-v2', 'HalfCheetah-v2', 'Hopper-v2', 'Walker2D-v2']
+envs = ['Ant-v2', 'Humanoid-v2', 'HalfCheetah-v2', 'Hopper-v2', 'Walker2d-v2']
 for env in envs:
     commands = []
     base_command = f'python main.py --env-name {env} --algo ppo --use-gae --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --entropy-coef 0 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 1000000 --use-linear-lr-decay --use-proper-time-limits --hidden_dim 64'
