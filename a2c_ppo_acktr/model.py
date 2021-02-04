2372a55380bb66b8a3ab198b4a3b9376a6d16f3d
Alex Li <alexlioralexli@gmail.com>
2021-02-04 02:28:32 -0800

    fixed typo

--- a2c_ppo_acktr/model.py
+++ a2c_ppo_acktr/model.py
@@ -227,11 +227,14 @@ class MLPBase(NNBase):
             network_class = FourierMLP
         del base_kwargs['network_class']
 
-        self.actor = MLP(n_hidden=base_kwargs['n_hidden'],
+        self.actor = MLP(num_inputs,
+                         hidden_size,
+                         n_hidden=base_kwargs['n_hidden'],
                          hidden_dim=base_kwargs['hidden_dim'],
-                         first_dim=base_kwargs['first_dim'],
-                         nonlinearity=base_kwargs['nonlinearity'])
-        print('only using network class for critic, not the actor!!!!')
+                         first_dim=base_kwargs.get('first_dim', 0),
+                         nonlinearity=base_kwargs['nonlinearity'],
+                         add_tanh=True)
+        print('only using network class for critic, not the actor!!!! Also assumes tanh' )
         # self.actor = network_class(num_inputs, hidden_size, **base_kwargs)
         self.critic = network_class(num_inputs, hidden_size, **base_kwargs)
         self.actor.apply(init_)
